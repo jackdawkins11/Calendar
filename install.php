2ee70f00045c47781dc4dbcaeff33b19105841a0
Jack <jackdawkins1974@gmail.com>
2020-03-15 15:46:59 -0500

    Fixed install file

--- install.php
+++ install.php
@@ -5,16 +5,18 @@ require "config.php";
 
 try{
 
-    $connection = new $PDO( "mysql:host=$host", $dbusername, $dbpassword );
+    $connection = new PDO( "mysql:host=$dbhost", $dbusername, $dbpassword );
 
     $query = "create database $dbname;"
         . " use $dbname;"
         . " create table users ( username varchar(100) not null, password varchar(100) not null);"
         . " create table fullnames ( username varchar(100) not null, fullname varchar(100) not null);"
-        . " create table events ( username varchar(100) not null, title varchar(100) not null,"
-        . " description varchar(200), datetime starttime, datetime endtime );";
+        . " create table events ( username varchar(100) not null, title varchar(100),"
+        . " description varchar(200), starttime datetime, endtime datetime );";
 
     $connection->exec( $query );
+
+    echo "Created the database";
 
 }catch( PDOException $e ){
     echo "Error installing database: " . $e->getMessage();
